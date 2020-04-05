2d0855081a31e15178968ee8bed321ad1da165cd
Khalid <17664047547>
2020-04-06 00:39:32 +0800

    主页及登录页面基本框架搭建

---
+++ MyBlogSite/MyBlogSite/Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace MyBlogSite.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
