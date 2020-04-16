6861fb4571a15b631978e5e0bfbea104b283f324
Khalid <17664047547>
2020-04-17 00:30:39 +0800

    新增主页内容,新增个人简历页面

---
+++ MyBlogSite/MyBlogSite/Controllers/ResumeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace MyBlogSite.Controllers
+{
+    public class ResumeController : Controller
+    {
+        // GET
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
